0c61d5569072f4333f2ac7ca28983167e4ea9c78
Jonathan Jackson <jonathan.jackson1@me.com>
2014-12-15 21:20:24 -0500

    Make jshint happy

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ var lockfileNames = {
 module.exports = {
   name: 'ember-cli-rails-addon',
   included: function(app) {
-    app.options.storeConfigInMeta = false
+    app.options.storeConfigInMeta = false;
   },
   preBuild: function(result) {
-    this.clearExistingLockfile(lockfileNames.post, result)
-    this.createLockfile(lockfileNames.pre, result)
+    this.clearExistingLockfile(lockfileNames.post, result);
+    this.createLockfile(lockfileNames.pre, result);
   },
   postBuild: function(result){
-    this.clearExistingLockfile(lockfileNames.pre, result)
-    this.createLockfile(lockfileNames.post, result)
+    this.clearExistingLockfile(lockfileNames.pre, result);
+    this.createLockfile(lockfileNames.post, result);
   },
   createLockfile: function(name, result) {
-    this.clearExistingLockfile(name, result)
-    fs.openSync(lockfile, 'w')
+    this.clearExistingLockfile(name, result);
+    fs.openSync(lockfile, 'w');
   },
   clearExistingLockfile: function(name, result){
     if(fs.existsSync(lockfile = path.join(result.directory, name))) {
-      fs.unlinkSync(lockfile)
+      fs.unlinkSync(lockfile);
     }
   }
 };
