c85e5d9e193d274304b97f6b86de312b95584647
Jonathan Jackson <jonathan.jackson1@me.com>
2014-12-17 12:18:11 -0500

    Use only one file to determine build status

--- index.js
+++ index.js
@@ -12,20 +12,16 @@ module.exports = {
     app.options.storeConfigInMeta = false;
   },
   preBuild: function(result) {
-    this.clearExistingLockfile(lockfileNames.post, result);
-    this.createLockfile(lockfileNames.pre, result);
+    if(!fs.existsSync(lockfile = this.lockfilePath())) {
+      fs.openSync(lockfile, 'w');
+    }
   },
   postBuild: function(result){
-    this.clearExistingLockfile(lockfileNames.pre, result);
-    this.createLockfile(lockfileNames.post, result);
-  },
-  createLockfile: function(name, result) {
-    this.clearExistingLockfile(name, result);
-    fs.openSync(lockfile, 'w');
-  },
-  clearExistingLockfile: function(name, result){
-    if(fs.existsSync(lockfile = path.join(result.directory, name))) {
+    if(fs.existsSync(lockfile = this.lockfilePath())) {
       fs.unlinkSync(lockfile);
     }
+  },
+  lockfilePath: function() {
+    return path.join(process.cwd(), 'tmp', 'build.lock');
   }
 };
