82eaaea3c7337d9c7f53799566c03e774c4f6b07
Sean Doyle <sean.p.doyle24@gmail.com>
2015-12-08 18:58:54 -0500

    WIP - :boom:

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
 var fs = require('fs');
-var fsExtra = require('fs-extra');
 var path = require('path');
-var urlJoin = require('url-join');
-
-function softSet(object, key, value) {
-  object = object || {};
-
-  if (typeof object[key] === 'undefined') {
-    object[key] = value;
-  }
-}
 
 module.exports = {
   name: 'ember-cli-rails-addon',
-  warnMissingDependencyChecker: function() {
-    var dependencies = this.project.dependencies();
-
-    if (!dependencies['ember-cli-dependency-checker']) {
-      console.warn('Usage of "ember-cli-dependency-checker" is strongly advised to ensure your project cache is in sync with the project\'s requirements.');
-    }
-  },
 
   init: function() {
-    this.warnMissingDependencyChecker();
     this.ensureTmp();
   },
 
@@ -30,53 +12,14 @@ module.exports = {
     fs.writeFileSync(this.errorFilePath(), error.stack)
   },
 
-  included: function(app) {
-    app.options.storeConfigInMeta = false;
-    softSet(app.options, 'fingerprint', {});
-    softSet(app.options, 'ember-cli-rails', {});
-    var addonOptions = app.options['ember-cli-rails'];
-
-    if (app.env !== 'test' && typeof process.env.RAILS_ENV !== 'undefined') {
-      var prefix = addonOptions.prefix || 'assets';
-      var assetHostWithProtocol = addonOptions.prepend || '';
-      var prepend = urlJoin(assetHostWithProtocol, prefix, app.name);
-
-      if (prepend.slice(-1) !== '/') {
-        prepend += '/';
-      }
-
-      softSet(app.options.fingerprint, 'enabled', true);
-      softSet(app.options.fingerprint, 'generateRailsManifest', true);
-      softSet(app.options.fingerprint, 'prepend', prepend);
-
-      if (app.env !== 'production') {
-        softSet(app.options.fingerprint, 'customHash', null);
-      }
-    }
-
-    if (process.env.EXCLUDE_EMBER_ASSETS) {
-      var excludeEmberAssets = process.env.EXCLUDE_EMBER_ASSETS;
-      var excludeRegex = new RegExp("(?:" + excludeEmberAssets.replace(",", "|") + ")\\.js$");
-      var excludeAssets = app.legacyFilesToAppend.filter(function(asset){ return excludeRegex.test(asset); });
-
-      excludeAssets.forEach(function(asset){
-        var index = app.legacyFilesToAppend.indexOf(asset);
-        app.legacyFilesToAppend.splice(index, 1);
-      });
-    }
-  },
   preBuild: function(result) {
     var lockFile = this.lockFilePath();
     var errorFile = this.errorFilePath();
     if(!fs.existsSync(lockFile)) { fs.openSync(lockFile, 'w'); }
     if(fs.existsSync(errorFile)) { fs.unlinkSync(errorFile); }
   },
-  postBuild: function(result){
-    fsExtra.copySync(
-      result.directory + '/index.html',
-      result.directory + '/assets/index.html'
-    );
 
+  postBuild: function(result){
     var lockFile = this.lockFilePath();
 
     if(fs.existsSync(lockFile)) {
@@ -90,12 +33,15 @@ module.exports = {
       fs.mkdirSync(dir);
     }
   },
+
   tmpDir: function() {
     return path.join(process.cwd(), 'tmp');
   },
+
   lockFilePath: function() {
     return path.join(this.tmpDir(), 'build.lock');
   },
+
   errorFilePath: function() {
     return path.join(this.tmpDir(), 'error.txt');
   }
