bdd194b26097865a9a08867b88be690f7d065062
joshuabremer <joshua.bremer@gmail.com>
2016-06-28 16:42:24 -0700

    Changed to match deprecation warning.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'ember-cli-rails-addon',
 
   init: function() {
-    this._super.apply(this, arguments);
+    this._super.init && this._super.init.apply(this, arguments);
     this.ensureTmp();
   },
 
